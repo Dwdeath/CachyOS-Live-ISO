9438fd57013315017e92a25c22c5850b869999fb
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-04-26 23:30:04 +0300

    Update profiledef.sh
    Changed name of iso

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="cachyos"
+iso_name="eddie_does_cachyos"
 iso_label="COS_$(date +%y%m)"
 iso_publisher="CachyOS <https://cachyos.org>"
 iso_application="CachyOS Live/Rescue CD"
